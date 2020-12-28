4454f05ef4e7750ccd7ab4482d76f682ad63b8f7
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2020-12-29 01:24:06 +0200

    java and html adjestings

---
+++ class02/java.js
@@ -0,0 +1,47 @@
+'use strict';
+var naame= prompt('What is your name?');
+console.log(naame);
+alert("Hi " + naame +" , my name is Bayan");
+alert("Welcome to my own website "+ naame + " hope taht you like it:)");
+alert("Lets play a short yeror no game about me^^");
+
+var userasnswer = prompt('Am I university graduate?yes/no');
+console.log(userasnswer);
+if (userasnswer.toLowerCase()==="yes"){
+    alert("YEESSS");
+}else{
+    alert("yes I am ^^");
+}
+
+var userans2 = prompt ('Is Black my favorate color ?yes/no');
+console.log(userans2);
+if (userans2.toLowerCase()==="yes"){
+    alert("You got it right");
+}else {
+    alert("Why not its the king of colors");
+}
+
+var userans3 =prompt("Am I a night pesron?yes/no");
+console.log(userans3);
+if (userans3.toLowerCase()==="yes"){
+    alert("No I don't like the dark");
+}else {
+    alert("from dusk till down :D");
+}
+
+var userans4 = prompt("Am I a reader?yes/no") ;
+console.log(userans4);
+if (userans4.toLowerCase()==="yes"){
+    alert("3 books a mounth ");
+}else{
+    alert("I love reading");
+}
+
+var userans5= prompt("Do I love pets? yes/no");
+console.log(userans5);
+if (userans5.toLowerCase()==="yes"){
+    alert("they are the cutest thing ever");
+}else{
+    alert("I adore them");
+}
+
